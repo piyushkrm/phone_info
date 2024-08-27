020fe5acf09d93c85a3b1975e5fc501f68c197ac
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-27 17:41:26 +0530

    pip freeze > requirements.txt

--- main.py
+++ main.py
@@ -10,12 +10,12 @@ phoneNumber = phonenumbers.parse(number)
  
 # printing the timezone using the timezone module
 timeZone = timezone.time_zones_for_number(phoneNumber)
-print("timezone : "+str(timeZone))
+print("Timezone : "+str(timeZone))
  
 # printing the geolocation of the given number using the geocoder module
 geolocation = geocoder.description_for_number(phoneNumber,"en")
-print("location : "+geolocation)
+print("Location : "+geolocation)
  
 # printing the service provider name using the carrier module
 service = carrier.name_for_number(phoneNumber,"en")
-print("service provider : "+service)
+print("Service provider : "+service)
